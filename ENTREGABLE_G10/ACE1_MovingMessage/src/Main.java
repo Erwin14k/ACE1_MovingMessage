333f76a79089a6b34df5aab8f4a79f705a2afb0f
Carlos Emilio Campos Morán <44531607+mr8ug@users.noreply.github.com>
2022-12-08 21:31:12 -0600

    ENTREGABLE

---
+++ ENTREGABLE_G10/ACE1_MovingMessage/src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        GuiModule loginModuleHandler = new GuiModule();
+        loginModuleHandler.initApp();
+
+    }
+}
